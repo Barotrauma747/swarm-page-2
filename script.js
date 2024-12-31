41db8f0e6ab0b92e35772afcb3a73d6d23b3ded9
Barotrauma747 <151362194+Barotrauma747@users.noreply.github.com>
2024-12-31 14:14:31 +0100

    Fix eventListener function for mouseover events

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 
 //Elements
-const albums = document.querySelector('.album');
+//const albums = document.querySelector('.');
+const albumList = document.querySelectorAll('.album')
 const itemForm = document.querySelectorAll('.item-form');
 const signBtn = document.querySelectorAll('.btn2');
 
+albumList.forEach(function (item) {
+    
+    item.addEventListener('mouseover', onMouseover)
+    item.addEventListener('mouseout', onMouseout)
+    
+    function onMouseover () {
+        item.style.borderColour = "#CCC";
+        item.style.border = "solid 1px";
+    };
+
+    function onMouseout () {
+        item.style.border = "0px";
+    };
+});
 
 
 
-function onMouseover () {
-    albums.style.borderColour = "#CCC"
-    albums.style.border = "solid 3px"
-};
 
-function onMouseout () {
-    albums.style.border = "0px";
-}
-
-console.log(albums);
-
-
-//Event Listeners
-albums.addEventListener('mouseover', onMouseover);
-albums.addEventListener('mouseout', onMouseout);
+console.log(albumList);
